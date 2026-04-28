a97ec572fd8a7fa4c8746a9eaa7a422df6ed193b
Mubeen Khan <mubix.dev@gmail.com>
2026-04-28 18:15:36 +0500

    add frontend url in cors origin

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://syncro-mern-chat-app-3of1.vercel.app",
     credentials: true,
   }),
 );
